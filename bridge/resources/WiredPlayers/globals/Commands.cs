12af782a7c7bee0ff03d707177026b205647f67c
Xabi <xabier1989@gmail.com>
2018-02-25 13:11:03 +0100

    Translation for admin system

---
+++ bridge/resources/WiredPlayers/globals/Commands.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace WiredPlayers.globals
+{
+    public class Commands
+    {
+        // Command names
+        public const String COMMAND_SKIN = "skin";
+        public const String COMMAND_ADMIN = "admin";
+        public const String COMMAND_COORD = "coord";
+        public const String COMMAND_TP = "tp";
+        public const String COMMAND_BRING = "bring";
+        public const String COMMAND_GUN = "gun";
+        public const String COMMAND_VEHICLE = "vehicle";
+        public const String COMMAND_GO = "go";
+        public const String COMMAND_BUSINESS = "business";
+        public const String COMMAND_CHARACTER = "character";
+        public const String COMMAND_HOUSE = "house";
+        public const String COMMAND_PARKING = "parking";
+        public const String COMMAND_POS = "pos";
+        public const String COMMAND_REVIVE = "revive";
+        public const String COMMAND_WEATHER = "weather";
+        public const String COMMAND_JAIL = "jail";
+        public const String COMMAND_KICK = "kick";
+        public const String COMMAND_KICKALL = "kickall";
+        public const String COMMAND_BAN = "ban";
+        public const String COMMAND_HEALTH = "health";
+        public const String COMMAND_SAVE = "save";
+        public const String COMMAND_ADUTY = "aduty";
+        public const String COMMAND_TICKETS = "tickets";
+        public const String COMMAND_ATICKET = "aticket";
+        public const String COMMAND_A = "a";
+        public const String COMMAND_RECON = "recon";
+        public const String COMMAND_RECOFF = "recoff";
+        public const String COMMAND_INFO = "info";
+        public const String COMMAND_POINTS = "points";
+
+        // Command arguments
+        public const String ARGUMENT_INFO = "info";
+        public const String ARGUMENT_CREATE = "create";
+        public const String ARGUMENT_MODIFY = "modify";
+        public const String ARGUMENT_COLOR = "color";
+        public const String ARGUMENT_DIMENSION = "dimension";
+        public const String ARGUMENT_FACTION = "faction";
+        public const String ARGUMENT_POSITION = "position";
+        public const String ARGUMENT_OWNER = "owner";
+        public const String ARGUMENT_REMOVE = "remove";
+        public const String ARGUMENT_REPAIR = "repair";
+        public const String ARGUMENT_LOCK = "lock";
+        public const String ARGUMENT_START = "start";
+        public const String ARGUMENT_BRING = "bring";
+        public const String ARGUMENT_TP = "tp";
+        public const String ARGUMENT_WORKSHOP = "workshop";
+        public const String ARGUMENT_ELECTRONICS = "electronics";
+        public const String ARGUMENT_POLICE = "police";
+        public const String ARGUMENT_TOWNHALL = "townhall";
+        public const String ARGUMENT_LICENSE = "license";
+        public const String ARGUMENT_VANILLA = "vanilla";
+        public const String ARGUMENT_HOSPITAL = "hospital";
+        public const String ARGUMENT_NEWS = "news";
+        public const String ARGUMENT_BAHAMA = "bahama";
+        public const String ARGUMENT_MECHANIC = "mechanic";
+        public const String ARGUMENT_GARBAGE = "garbage";
+        public const String ARGUMENT_NAME = "name";
+        public const String ARGUMENT_TYPE = "type";
+        public const String ARGUMENT_BANK = "bank";
+        public const String ARGUMENT_MONEY = "money";
+        public const String ARGUMENT_JOB = "job";
+        public const String ARGUMENT_RANK = "rank";
+        public const String ARGUMENT_INTERIOR = "interior";
+        public const String ARGUMENT_PRICE = "price";
+        public const String ARGUMENT_STATE = "state";
+        public const String ARGUMENT_RENT = "rent";
+        public const String ARGUMENT_HOUSE = "house";
+        public const String ARGUMENT_PLACES = "places";
+        public const String ARGUMENT_GIVE = "give";
+        public const String ARGUMENT_SET = "set";
+    }
+}
